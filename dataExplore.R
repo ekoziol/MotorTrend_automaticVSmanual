cdd76513cba3e3eafad86bc869e7434368f2e77a
Eric Koziol <eric@ISs-MacBook-Air.local>
2014-07-27 09:42:26 -0500

    Finished Report

--- dataExplore.R
+++ dataExplore.R
@@ -7,6 +7,9 @@ data$am[data$am == 0] <- "automatic"
 data$am[data$am == 1] <- "manual"
 auto <- data[data$am == "automatic",]
 manual <- data[data$am == "manual",]
+data$ev2 <- data$hp * data$cyl * data$wt / data$disp
+data$ev <- data$hp * data$cyl * data$wt
+
 
 qplot(cyl, mpg, data=data, col= am)
 ggplot(data, aes(x=mpg, color=am)) + geom_density()
